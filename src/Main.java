3af0d7e3eb886b1932a6a48940d2855e17fc060a
Sergey Ivanov <Reger21a@yandex.ru>
2024-01-24 11:24:29 +0300

    Алгорим перевода в польскую нотацию

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        OrdinaryCalculator test = new OrdinaryCalculator("(1+2)*(1+2)");
+        OrdinaryCalculator test = new OrdinaryCalculator("((1+2)*(3+4))+10"); //(1+2)*8*(4+6) не работает
+        //Правильный ответ 1 2 + 8 * 4 6 + *
         System.out.println(test.makeInvertPolandNotation());
 
-
+//((1+2) * (3+4)) + 10
     }
 }
--- src/OrdinaryCalculator.java
+++ src/OrdinaryCalculator.java
@@ -19,21 +19,52 @@ public class OrdinaryCalculator implements Calculator {
             for (int j = 0; j < mathSymbols.length; j++) {
                 if (arrExpression[i] == mathSymbols[j]) {
                     isThereMathSymbol = true;
-                    stack.push(mathSymbols[j]);
-                    checkStack();
+                    addToStack(mathSymbols[j]);
                     break;
                 }
             }
             if (!isThereMathSymbol) {
                 output = output + arrExpression[i];
             }
+
         }
+        if (!stack.getStack().isEmpty()) {
+            output = output + stack.pop();
+        }
+
         return output;
 
     }
 
+    public void addToStack(char symbol) {
+        if (stack.getStack().isEmpty()) {
+            stack.push(symbol);
+            return;
+        }
+        for (Character sym : stack.getStack()) {
+            if ((sym == symbol) && ((symbol != ')') && (symbol != '('))) {
+                output = output + symbol;
+                return;
+            }
+        }
+        if ((symbol == ')') && (stack.getStack().contains('('))) {
+            for (int i = 0; i < stack.getStack().size(); i++) {
+                char sym = stack.pop();
+                if ((sym != ')') && (sym != '(')) {
+                    output = output + sym;
+                }
+            }
+            stack.clean();
+            return;
+        }
+        stack.push(symbol);
+    }
 
-    public void checkStack() {
+
+
+
+
+/*    public void checkStack() {
         if (stack.getStack().contains('(') && stack.getStack().contains(')')) {
             for (int i = 0; i < stack.getStack().size(); i++) {
                 char symbol = stack.pop();
@@ -42,7 +73,31 @@ public class OrdinaryCalculator implements Calculator {
                 }
             }
             stack.clean();
-        }
-    }
+        }*/
+/*        if (stack.getStack().size() > 1) {
+            if ((stack.getStack().get(0) != null) && (stack.getStack().get(1) != null)) {
+                if (stack.getStack().get(0) == stack.getStack().get(1)) {
+                    output = output + stack.pop();
+                }
+            }
+        }*/
+
+
+
+
 
 }
+/*
+else if (stack.getStack().get(0) == stack.getStack().get(1)) {
+        output = output + stack.pop();
+        }*/
+
+
+/*
+        if (stack.getStack().size() > 1) {
+                if ((stack.getStack().get(0) != null) && (stack.getStack().get(1) != null)) {
+                if (stack.getStack().get(0) == stack.getStack().get(1)) {
+                output = output + stack.pop();
+                }
+                }
+                }*/
