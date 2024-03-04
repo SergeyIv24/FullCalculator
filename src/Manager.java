8290618bd60388ce2ae1e756b81fa82abbabc4ff
Sergey Ivanov <Reger21a@yandex.ru>
2024-03-04 16:51:51 +0300

    Добавить отлов исключений

--- src/Manager.java
+++ src/Manager.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.SortedMap;
 
@@ -6,26 +7,30 @@ public class Manager {
     CheckerInput checkerInput;
     OrdinaryCalculator calculator;
 
-    public void interactionWithUser() {
+    public void userMenu() {
         System.out.println("Выберите, действие: ");
         System.out.println("1 - Решить выражение.");
         System.out.println("2 - Перевод выражения в обратную польскую запись.");
         System.out.println("3 - Перевод в другую систему счисления.");
         System.out.println("4 - Вычислить выражение в другой СИ");
-        System.out.println("5 - Выход");
-
+        System.out.println("5 - Перевод десятичной системы счисления в римскую");
+        System.out.println("6 - Перевод из римской системы счисления в десятичную");
+        System.out.println("7 - Выход");
+    }
+    public void interactionWithUser() {
 
         int selectionCalculator = 0;
-        try {
-            if (scanner.hasNextInt()) {
-                selectionCalculator = scanner.nextInt();
-            } else {
-                throw new UserInoutException("Введено не число");
+        while (true) {
+            try {
+                userMenu();
+                selectionCalculator = Integer.parseInt(scanner.next());
+                break;
+            } catch (Exception exception) {
+                System.out.println("Введено не число!");
             }
-        } catch (UserInoutException exception) {
-            System.out.println(exception.getMessage());
-            scanner.close();
         }
+
+
 
 
         switch (selectionCalculator) {
@@ -53,21 +58,40 @@ public class Manager {
                 System.out.println("Отрицательные числа вводить в скобках - (-5)");
                 String expressionForPoland = scanner.next();
                 checkerInput = new CheckerInput(expressionForPoland);
-                boolean checkBracketsExp = checkerInput.checkBrackets();
-                String exp1 = checkerInput.convertUsualMinusToUnaryMinus();
-                if (checkBracketsExp) {
+                try {
+                    checkerInput.checkInput();
+                    String exp1 = checkerInput.convertUsualMinusToUnaryMinus();
                     calculator = new OrdinaryCalculator(exp1);
                     System.out.println("Результат: " + calculator.makeInvertPolandNotation());
-                } else {
-                    System.out.println("Проверьте ввод и попробуйте еще раз!");
+                } catch (UserInoutException exception) {
+                    System.out.println(exception.getMessage());
+                } finally {
+                    interactionWithUser();
                 }
                 break;
 
             case 3:
-                System.out.println("Введите систему счисления из которой нужен перевод: ");
-                int systemFrom = scanner.nextInt();
-                System.out.println("Введите систему счисления в которую нужно перевести: ");
-                int systemTo = scanner.nextInt();
+
+                int systemFrom = 0;
+                while (true) {
+                    try {
+                        System.out.println("Введите систему счисления из которой нужен перевод: ");
+                        systemFrom = Integer.parseInt(scanner.next());
+                        break;
+                    } catch (Exception e) {
+                        System.out.println("Неверный ввод");
+                    }
+                }
+                int systemTo = 0;
+                while (true) {
+                    try {
+                        System.out.println("Введите систему счисления в которую нужно перевести: ");
+                        systemTo = Integer.parseInt(scanner.next());
+                        break;
+                    } catch (Exception e) {
+                        System.out.println("Неверный ввод");
+                    }
+                }
 
                 System.out.println("Введите число: ");
 
@@ -85,13 +109,30 @@ public class Manager {
                 break;
 
             case 4:
-                System.out.println("Введите математическое выражение для расчета:");
+                int userSystem = 0;
+                while (true) {
+                    System.out.println("Укажите систему счисления в которой нужно получить результат:");
+                    try {
+                        userSystem = Integer.parseInt(scanner.next());
+                        break;
+                    } catch (Exception e) {
+                        System.out.println("Введено не число!");
+                    }
+                }
+
+                System.out.println("Введите математическое выражение для расчета (в десятичной системе:)");
                 String userExp = scanner.next();
-                System.out.println("Укажите систему счисления в которой нужно получить результат:");
-                int userSystem = scanner.nextInt();
-                System.out.println(ConvertToDifferentSystem.calculateTotal(userExp, userSystem));
+                checkerInput = new CheckerInput(userExp);
+                try {
+                    checkerInput.checkInput();
+                    System.out.println(ConvertToDifferentSystem.calculateTotal(userExp, userSystem));
+                } catch (UserInoutException exception) {
+                    System.out.println(exception.getMessage());
+                } finally {
+                    interactionWithUser();
+                }
                 break;
-            case 5:
+            case 7:
                 System.out.println("Выход...");
                 break;
             default:
