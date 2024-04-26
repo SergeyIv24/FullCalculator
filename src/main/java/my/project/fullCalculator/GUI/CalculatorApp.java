75be159a9ea6040d09fd90125d6fe4131bc0e2e0
Sergey Ivanov <Reger21a@yandex.ru>
2024-04-26 15:51:21 +0300

    Фикс предупреждений CalculatorApp

--- src/main/java/my/project/fullCalculator/GUI/CalculatorApp.java
+++ src/main/java/my/project/fullCalculator/GUI/CalculatorApp.java
@@ -8,23 +8,28 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.Objects;
 
 public class CalculatorApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoaderMenu = new FXMLLoader(CalculatorApp.class.getResource("/menu-view.fxml")); //Загрузка XML файла
         Parent rootNode = fxmlLoaderMenu.load(); //Корневой узел
-        Scene sceneMenu = new Scene(rootNode, 500, 500); // Создание сцены по корневому узлу
-
-        String pathToCss = this.getClass().getResource("/styleMenu.css").toExternalForm();
-        sceneMenu.getStylesheets().add(pathToCss);
-
+        Scene sceneMenu = new Scene(rootNode, 600, 600); // Создание сцены по корневому узлу
+        String pathToCss = "";
+        try { //Путь к файлу css
+            pathToCss = Objects.requireNonNull(this.getClass().getResource("/styleMenu.css")).toExternalForm();
+        } catch (NullPointerException e) {
+            e.getMessage();
+        }
+        sceneMenu.getStylesheets().add(pathToCss); //Подключение стилей
         stage.setTitle("Калькулятор"); //Заголовок окна
-
         InputStream iconStream = CalculatorApp.class.getResourceAsStream("/calc-icon.png");
-        Image iconApp = new Image(iconStream);
-        stage.getIcons().add(iconApp);
-
+        Image iconApp = null;
+        if (iconStream != null) {
+            iconApp = new Image(iconStream);
+        }
+        stage.getIcons().add(iconApp); //Установка иконки приложения
         stage.setScene(sceneMenu);
         stage.show();
     }
